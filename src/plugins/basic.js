39bf520e3b6f7c14f09e0d4cfb7cf7b6207cbfa6
unknown <nicolito128@hotmail.com>
2020-04-23 01:09:39 -0300

    Update basic.js

--- src/plugins/basic.js
+++ src/plugins/basic.js
@@ -1,7 +1,7 @@
 const github = 'https://github.com/nicolito128/Muphasa-bot'
 
 exports.commands = {
-    github({message, by, args, cmd}) {
-        message.channel.send(`¡Hola, ${by}! Soy **${Config.name}** y fui desarrollado por ${message.member.toString(Config.ownerId)}. Mi código: ${github}`)
+    github({message, user, args, cmd}) {
+        this.channel.send(`¡Hola, ${user}! Soy **${Config.name}** y fui desarrollado por ${message.member.toString(Config.ownerId)}. Mi código: ${github}`)
     }
 }
