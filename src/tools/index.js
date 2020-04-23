abc6f4d62dcb4c52c8092e64e88b31bf5e3f9a72
unknown <nicolito128@hotmail.com>
2020-04-23 16:37:03 -0300

    Add embed

--- src/tools/index.js
+++ src/tools/index.js
@@ -2,6 +2,12 @@
  * File to exports all the tools
  */
 
+const ID = require('./id.js')
+const Embed = require('./embed.js')
+
 module.exports = {
-    ID: require('./id.js')
+    ID,
+    toID: ID.prototype.makeId,
+
+    Embed
 }
