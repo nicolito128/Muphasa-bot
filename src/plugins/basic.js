5cd8db69316736ce0253eef79f8eb9ef062597fe
unknown <nicolito128@hotmail.com>
2020-04-24 00:05:29 -0300

    Add help & Update github command

--- src/plugins/basic.js
+++ src/plugins/basic.js
@@ -1,7 +1,33 @@
 const github = 'https://github.com/nicolito128/Muphasa-bot'
 
-exports.commands = {
+module.exports.commands = {
+    help({message, user, args, cmd}) {
+        const helps = Plugins.getHelp()
+
+        if (!args[0]) return this.channel.send('Ingresa un comando del cual quieras obtener información.')
+
+        const target = args[0].toLowerCase()
+        if (!helps[target]) return this.channel.send('No hay ayuda disponible sobre este comando o no existe.')
+
+        this.channel.send(
+            Tools.Embed.notify(
+                '',
+                `**${Config.prefix}${target}**: ${helps[target]}`
+            )
+        )
+    },
+
     github({message, user, args, cmd}) {
-        this.channel.send(`¡Hola, ${user}! Soy **${Config.name}** y fui desarrollado por ${message.member.toString(Config.ownerId)}. Mi código: ${github}`)
-    }
+        this.channel.send(
+            Tools.Embed.notify(
+                'Github',
+                `¡Hola, ${user}! Soy **${Config.name}** y fui desarrollado por ${this.member.toString(Config.ownerId)}. Todavía me encuentro en fase de pruebas, ¡Pero no dudes en contar conmigo como tu BOT de confianza!`,
+                [68, 197, 76]
+                ).setURL(github)
+        )
+    },
+}
+
+module.exports.help = {
+    github: 'Muestra el enlace al código fuente del BOT y datos sobre el desarrollo.'
 }
