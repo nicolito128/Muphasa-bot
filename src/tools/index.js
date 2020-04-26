988f623c19763ed25441508b9d3207aa91f424ee
unknown <nicolito128@hotmail.com>
2020-04-26 17:05:30 -0300

    Remove game

--- src/tools/index.js
+++ src/tools/index.js
@@ -7,6 +7,7 @@ const PROJECT_ROOT = __dirname + '/../../'
 // requires
 const ID = require('./id.js')
 const Embed = require('./embed.js')
+const Game = require('./game.js')
 
 // functions
 const parsePath = path => PROJECT_ROOT + path
@@ -14,8 +15,6 @@ const parsePath = path => PROJECT_ROOT + path
 module.exports = {
     ID,
     toID: ID.prototype.makeId,
-
     Embed,
-
     parsePath
 }
