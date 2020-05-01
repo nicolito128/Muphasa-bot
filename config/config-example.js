c57c2abe157af194bdc9f9e9cced7497c69e026e
unknown <nicolito128@hotmail.com>
2020-05-01 00:44:40 -0300

    Add strict mode

--- config/config-example.js
+++ config/config-example.js
@@ -4,6 +4,7 @@
  * All important data ​​for the bot and connection are saved here
  * When run the bot this file will be used to create config.js
 */
+'use strict';
 
 // Token is required to connect to client
 exports.token = ""
